71552da57b229618b1087ebfb842c606172d7039
Maria Mayskaya <mashamay@gmail.com>
2015-11-19 21:25:52 +0300

    state does not update to posted

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
+  devise_for :users, controllers: {registrations: 'registrations'}
+
   authenticated :user, -> user { user.admin? } do
     mount Delayed::Web::Engine, at: '/jobs'
   end
+
   get 'auth/:provider/callback', to: 'connections#create'
   resources :connections, only: [:destroy]
+  
   get 'auth/failure', to: 'connections#omniauth_failure'
-
-  devise_for :users, controllers: {registrations: 'registrations'}
   root 'pages#home'
   get 'dashboard', to: "pages#dashboard"
 
