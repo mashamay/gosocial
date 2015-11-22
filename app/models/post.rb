fda32cf6fa22cd50d8e34aaf4cf4de8dc4592604
Maria Mayskaya <mashamay@gmail.com>
2015-11-22 21:14:54 +0300

    Made background jobs work

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   validates_presence_of :content
   validates_presence_of :scheduled_at
   validates_length_of :content, maximum: 140, message: "less than 140 please"
-  validates_datetime :scheduled_at, :on_or_after => Time.zone.now
+  validates_datetime :scheduled_at, :on => :create, :on_or_after => Time.zone.now
   after_create :schedule
 
   def schedule
